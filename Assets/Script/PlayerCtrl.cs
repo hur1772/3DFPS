967995eb8a762cc1f3eb6054c3e7c769e6bbb31f
hur1772 <hur1772@naver.com>
2022-08-29 19:06:08 +0900

    .

---
+++ Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        idle,
+        move,
+        run,
+        zoom,
+        shot,        
+        death
+    }
+
+    public PlayerState playerstate = PlayerState.idle;
+
+    //탱크의 이동 및 회전 속도를 나타내는 변수
+    public float moveSpeed = 20.0f;
+    public float rotSpeed = 50.0f;
+
+    //참조할 컴포넌트를 할당할 변수
+    private Rigidbody rbody;
+
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX;
+
+    [SerializeField]
+    private Camera theCamera;
+
+    bool isGround = true;
+
+    bool isRun = false;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+
+        //Rigidbody의 무게중심을 낮게 설정
+        rbody.centerOfMass = new Vector3(0.0f, -2.5f, 0.0f);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (isGround)
+        {
+            rbody.drag = 30;
+        }
+        else
+            rbody.drag = 0;
+
+        switch(playerstate)
+        {
+            case PlayerState.idle:
+
+                break;
+            case PlayerState.move:
+                Move(walkSpeed);
+                break;
+            case PlayerState.run:
+                Run();
+                break;
+            case PlayerState.shot:
+
+                break;
+            case PlayerState.zoom:
+
+                break;
+            case PlayerState.death:
+
+                break;
+        }
+
+        stateCheck();
+        
+        CameraRotation();      
+        CharacterRotation();    
+    }
+
+    private void Move(float speed)
+    {
+    //    h = Input.GetAxis("Horizontal");
+    //    v = Input.GetAxis("Vertical");
+
+    //    //회전과 이동처리
+    //    tr.Rotate(Vector3.up * rotSpeed * h * Time.deltaTime);
+    //    tr.Translate(Vector3.forward * v * moveSpeed * Time.deltaTime);
+
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        rbody.MovePosition(transform.position + _velocity* Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        Move(walkSpeed + 10);
+    }
+
+    private void CameraRotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+    private void stateCheck()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0|| Input.GetAxisRaw("Vertical") != 0)
+        {
+            if(!isRun)
+                playerstate = PlayerState.move;
+        }
+        else
+        {
+            playerstate = PlayerState.idle;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            isRun = true;
+            playerstate = PlayerState.run;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRun = false;
+        }
+    }
+
+    private void CharacterRotation()  // 좌우 캐릭터 회전
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        rbody.MoveRotation(rbody.rotation * Quaternion.Euler(_characterRotationY)); // 쿼터니언 * 쿼터니언
+        // Debug.Log(myRigid.rotation);  // 쿼터니언
+        // Debug.Log(myRigid.rotation.eulerAngles); // 벡터
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGround = false;
+        }
+    }
+}
